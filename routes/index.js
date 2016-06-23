6ce5040b6da8d6e50947df320e46032240d25d54
Akash <akash.mishra@innoplexus.com>
2016-06-23 13:12:51 +0530

    created the post route for db

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,23 @@ router.get('/posts', function(req, res, next){
   });
 });
 
+//we need to make sure that mongoose is imported and that we have handles to the
+// Post and Comment models. Then we use the express get() method to define the URL
+ // for the route (/posts) and a function to handle the request. Inside our request handler,
+ //  we query the database for all posts. If and error occurred, we pass the error to an error
+ //   handling function otherwise we use res.json() to send the retrieved posts back to the client.
+
+
+router.post('/posts', function(req, res, next){
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+
 module.exports = router;
